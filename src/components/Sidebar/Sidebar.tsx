f27e217ca276fd1e9c669cb0576ee53161316bd1
gustafo01 <vakansson@gmail.com>
2023-02-04 16:15:10 +0200

    test

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Sidebar = () => {
+  console.log("test")
   return (
     <div className="sidebar">
       <ul className="sidebarNav">
